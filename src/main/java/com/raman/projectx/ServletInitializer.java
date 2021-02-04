65f9b059abb0ba89ba96134cb63f8e970cd6a1cb
Ramanareddy Palugulla <ramanreddy2222@gmail.com>
2021-02-04 23:49:41 +0530

    first commit

---
+++ src/main/java/com/raman/projectx/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.raman.projectx;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectXApplication.class);
+	}
+
+}
---
+++ src/main/java/com/raman/projectx/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.raman.projectx.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/projectx/")
+public class HomeController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to ProjectX";
+	}
+
+}
